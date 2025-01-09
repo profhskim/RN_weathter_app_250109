f1fc30bfebb57099f43da2f5e48df31a1392f78b
profhskim <profhskim2@gmail.com>
2025-01-09 16:22:55 +0900

    Weather App Desing

--- App.js
+++ App.js
@@ -2,15 +2,76 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react'
 
-export default function App() {
+const App = ()=>{
   return (
-    <View style={{flex:1,}}>
-      <View style={{flex:1, backgroundColor:"red"}}></View>
-      <View style={{flex:2, backgroundColor:"darkorange"}}></View>
-      <View style={{flex:3, backgroundColor:"green"}}></View>
-      <StatusBar style='auto'/>
+    <View style={styles.container}>
+      <View style={styles.cityCon}>
+        <Text style={styles.city}>Ansan</Text>
+      </View>
+      <View style={styles.weatherCon}>
+        <View style={styles.day}>
+          <Text style={styles.regDate}>1월 9일, 목, 15:51</Text>
+          <Text style={styles.desc}>맑음</Text>
+        </View>
+        <View style={styles.tempCon}>
+          <Text style={styles.temp}>-9도</Text>
+        </View>
+      </View>
+       <StatusBar style='auto'/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: "#91a51c"
+  },
+  cityCon: {
+    flex:1,
+  },
+  city:{
+    flex:1,
+    marginTop:50,
+    paddingTop:20,
+    fontSize: 40,
+    textAlign:"center",
+    fontWeight:"bold",
+  },
+  weatherCon:{
+    flex:3,
+  },
+  day: {
+    flex:0.2,
+    alignItems:"center",
+    justifyContent: "center",
+
+   },
+  regDate: {
+    paddingTop:10,
+    paddingLeft:20,
+    paddingRight:20,
+    paddingBottom:15,
+    backgroundColor:"black",
+    color:"white",
+    fontWeight:"bold",
+    borderRadius:20,
+    overflow: "hidden",
+  },
+  desc: {
+    flex:1.5,
+    marginTop:20,
+    fontSize:25,
+    fontWeight:"bold",
+  },
+  tempCon: {
+    flex:0.3,
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  temp:{
+    fontSize:120,
+  }
+});
+
+export default App;
