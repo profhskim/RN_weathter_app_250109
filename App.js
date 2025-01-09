5d4c5e89d909e534628d97667a311495d129ffc6
profhskim <profhskim2@gmail.com>
2025-01-09 15:00:33 +0900

    Flex Box Usage

--- App.js
+++ App.js
@@ -3,35 +3,14 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react'
 
 export default function App() {
-  const [number, setNumber] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>결과: {number}</Text>
-      <View style={styles.btnGroup}>
-        <Button style={styles.btn} title="증가" onPress={()=>{setNumber(number+1)}}/>
-        <Button style={styles.btn} title="감소" onPress={()=>{setNumber(number-1)}}/>
-      </View>
+    <View style={{flex:1,}}>
+      <View style={{flex:1, backgroundColor:"red"}}></View>
+      <View style={{flex:2, backgroundColor:"darkorange"}}></View>
+      <View style={{flex:3, backgroundColor:"green"}}></View>
       <StatusBar style='auto'/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 50,
-    fontWeight:"bold",
-  },
-  btnGroup: {
-    flexDirection:"row",
-    gap:10,
-  },
-  btn: {
-    flex:2, 
-  }
-});
+const styles = StyleSheet.create({});
